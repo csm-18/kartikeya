63d13bcd24aad64cc7d497092884129654f1a9ed
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-03-23 23:31:46 +0530

    compiler: basic cli handling
    handling user entered commands and args

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,5 +1,41 @@
+use std::process::exit;
+
+
+const VERSION:&str = "1.0.0";
 fn main() {
+    //cli args
     let args: Vec<String> = std::env::args().collect();
 
-    dbg!(&args);
+    //cli handling
+    if args.len() == 1{
+        println!("kc {VERSION}");
+        println!("the compiler for kartikeya language");
+        println!("\nfor usage:\n kc help");
+    }else if args.len() == 2 {
+        if args[1] == "version" || args[1] == "--version" || args[1] == "-v" {
+            println!("kc {VERSION}");
+        }else if args[1] == "help" || args[1] == "--help" || args[1] == "-h" {
+            println!("kc commands:");
+            println!(" 1. kc <no args>    -> prints about the compiler");
+            println!(" 2. kc version      -> prints the version of the compiler");
+            println!(" 3. kc help         -> prints this commands list");
+            println!(" 4. kc <filename.k> -> compiles k source file into c source file");
+            
+        }else if args[1].len() >= 3{
+            let filename = &args[1];
+            if &filename[filename.len()-2..] != ".k" {
+                println!("error: invalid source file!");
+                exit(1);
+            }
+            dbg!(&args[1]);
+        }else {
+            println!("error: invalid command!");
+            exit(1);
+        }
+    }else {
+        println!("error: too many arguments!");
+        exit(1);
+    }
+
+
 }
