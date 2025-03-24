8fb2307f5eac262aee6f36a18e4e066c4905f6cf
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-03-24 23:37:22 +0530

    add read_file() function to compiler module in kc package
    this function reads a file to string

--- kc/src/compiler/mod.rs
+++ kc/src/compiler/mod.rs
@@ -1,3 +1,19 @@
+use std::{path::Path, process::exit};
+
 pub fn compile(filename:&str){
-    println!("compiling file: {}", filename);
+    let code = read_file(filename);
+    println!("'{filename}' contains:\n {code}");
+}
+
+fn read_file(filename:&str)-> String{
+    let file_path = Path::new(filename);
+    let content = match std::fs::read_to_string(file_path){
+        Ok(content) => content,
+        Err(_) => {
+            println!("error: '{filename}' not found!");
+            exit(1);
+        }
+        
+    }; 
+    content
 }
